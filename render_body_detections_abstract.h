b1ec60a880f17f4dd19e0912dc63e1628fa3f7cf
Karla Trejo <karla.3jo@gmail.com>
2018-10-14 13:48:54 +0200

    Update render_body_detections_abstract.h

--- render_body_detections_abstract.h
+++ render_body_detections_abstract.h
@@ -3,13 +3,20 @@
 #undef DLIB_RENDER_FACE_DeTECTIONS_ABSTRACT_H_
 #ifdef DLIB_RENDER_FACE_DeTECTIONS_ABSTRACT_H_
 
+/* This library was modified from Dlib's render_face_detections_abstract.h.
+Copy and paste this file in Dlib's image_processing folder. If you
+encounter any error when running the main file of this repository, comment 
+the original contents of render_face_detections_abstract.h and copy-paste 
+the contents of this file, changing all the "body" words for "face".
+*/
+
 #include "full_object_detection_abstract.h"
 #include "../gui_widgets.h"
 
 namespace dlib
 {
 
-    inline std::vector<image_window::overlay_line> render_face_detections (
+    inline std::vector<image_window::overlay_line> render_body_detections (
         const std::vector<full_object_detection>& dets,
         const rgb_pixel color = rgb_pixel(0,255,0)
     );
@@ -22,7 +29,7 @@ namespace dlib
               the karla3jo body landmark scheme.  We then return a set of overlay lines that
               will draw the objects onto the screen in a way that properly draws the
               outline of the human body shape defined by the part locations.
-            - returns a vector with dets.size() elements, each containing the lines
+            - Returns a vector with dets.size() elements, each containing the lines
               necessary to render a people detection from dets.
     !*/
 
